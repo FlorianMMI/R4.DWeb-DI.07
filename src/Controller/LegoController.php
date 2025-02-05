36e12df9e89675dc921ce18e58d99e046d633676
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-02-05 16:13:03 +0100

    Tp3 Exercice 1

--- src/Controller/LegoController.php
+++ src/Controller/LegoController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Lego;
-
+use App\Service\LegoService;
 
 /* le nom de la classe doit être cohérent avec le nom du fichier */
 class LegoController extends AbstractController
@@ -25,19 +25,9 @@ class LegoController extends AbstractController
     //     return $this->render('test.html.twig', ['msg' => $msg]); // Il part du pricipe que le fichier twig est dans le dossier templates
     // }
 
-    public function home() : Response
+    public function home(LegoService $legoService) : Response
     {
-        $lego = new Lego(
-            10252,
-            "La coccinelle Volkwagen",
-            "Creator Expert"
-        );
-        $lego->setDescription("Construis une réplique LEGO® Creator Expert de l'automobile la plus populaire au monde. Ce magnifique modèle LEGO est plein de détails authentiques qui capturent le charme et la personnalité de la voiture, notamment un coloris bleu ciel, des ailes arrondies, des jantes blanches avec des enjoliveurs caractéristiques, des phares ronds et des clignotants montés sur les ailes.");
-        $lego->setPrice(94.99);
-        $lego->setPieces(1167);
-        $lego->setboxImage("LEGO_10252_Box.png");
-        $lego->setlegoImage("LEGO_10252_Main.jpg");
-
+        $lego = $legoService->getLego();
         return $this->render('lego.html.twig', ['lego' => $lego]);
     }
 
